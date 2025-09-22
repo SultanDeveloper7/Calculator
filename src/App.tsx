7afaaf5a4ffa0f59b069694bbcf8dbc43c76a14b
sultanalpha <sultanalpha82@gmail.com>
2025-09-22 22:56:46 +0300

    Added light and dark mode functionality with localstorage storing

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,26 @@
-import { useState } from "react";
+import {  useState } from "react";
 import sunIcon from "../public/icons/icons8-sun-50.png";
 import moonIcon from "../public/icons/icons8-moon-50.png";
 import "./App.css";
 
 export default function App() {
+  const themeDuration = "transition-colors duration-500 ease-in-out";
   const [result, setResult] = useState<string>("");
   const [display, setDisplay] = useState<string>("");
+
+  function toggleTheme(isDark: boolean) {
+    localStorage.setItem("isDark", isDark ? "dark" : "light");
+    if (isDark) {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
+  }
 
   function appendDisplay(value: string) {
     setDisplay(display + value);
   }
+
   function clearDisplay() {
     setDisplay("");
     setResult("");
@@ -24,8 +35,6 @@ export default function App() {
     console.log(`The result value is: ${result}`);
     console.log(`The display type is: ${typeof display}`);
     console.log(`The result type is: ${typeof result}`);
-    // console.log(`The last number value is: ${lastNumber}`);
-    // console.log(`The operator value is: ${op}`);
     console.log(`------------------------------------------------`);
   }
 
@@ -35,17 +44,17 @@ export default function App() {
   }
 
   return (
-    <div className="main-body">
-      <div className="bg-[#030822] rounded-[20px] border-[#040c35] border-4 overflow-hidden">
-        <div className="h-12 flex items-center justify-center">
-          <div className="border-white border-1 border-solid rounded-[6px] px-3 py-2 flex gap-6">
-            <img src={sunIcon} height={22} width={22} />
-            <img src={moonIcon} height={22} width={22} />
+    <div className={`bg-[#E6E6E6] dark:bg-[#1b1b1f] ${themeDuration} h-[100vh] w-[100%] flex items-center justify-center`}>
+      <div className={`bg-[#FFFFFF] dark:bg-[#030822] ${themeDuration} rounded-[20px] overflow-hidden`}>
+        <div className="h-12 mb-8 flex items-center justify-center">
+          <div className={`bg-[#F0F0F0] dark:bg-[#1E2032] ${themeDuration} rounded-[6px] px-3 py-2 flex gap-6`}>
+            <img src={sunIcon} height={22} width={22} onClick={() => toggleTheme(false)} />
+            <img src={moonIcon} height={22} width={22} onClick={() => toggleTheme(true)} />
           </div>
         </div>
         <div className="flex h-8 items-center px-2 text-white overflow-hidden tracking-widest">{display}</div>
         <div className="flex h-8 items-center px-2 text-[#138EA4] font-bold overflow-hidden">{result}</div>
-        <div className="grid bg-[#181C2F] p-2 rounded-t-[18px] grid-cols-4 gap-1">
+        <div className={`grid bg-[#F0F0F0] dark:bg-[#181C2F] ${themeDuration} p-2 rounded-t-[18px] grid-cols-4 gap-1`}>
           <button className="text-[#1759A4]" onClick={clearDisplay}>
             AC
           </button>
@@ -55,52 +64,52 @@ export default function App() {
           <button className="text-[#1759A4]" onClick={() => appendDisplay("%")}>
             %
           </button>
-          <button className="text-[#138EA4] bg-[#1F2130]" onClick={() => appendDisplay("+")}>
+          <button className={`text-[#138EA4] bg-[#F8FAF9] dark:bg-[#1F2130] ${themeDuration}`} onClick={() => appendDisplay("+")}>
             +
           </button>
-          <button className="numbers" onClick={() => appendDisplay("7")}>
+          <button className={`dark:text-[#b3b6be] ${themeDuration} text-black`} onClick={() => appendDisplay("7")}>
             7
           </button>
-          <button className="numbers" onClick={() => appendDisplay("8")}>
+          <button className={`dark:text-[#b3b6be] ${themeDuration} text-black`} onClick={() => appendDisplay("8")}>
             8
           </button>
-          <button className="numbers" onClick={() => appendDisplay("9")}>
+          <button className={`dark:text-[#b3b6be] ${themeDuration} text-black`} onClick={() => appendDisplay("9")}>
             9
           </button>
-          <button className="text-[#138EA4] bg-[#1F2130]" onClick={() => appendDisplay("-")}>
+          <button className={`text-[#138EA4] bg-[#F8FAF9] dark:bg-[#1F2130] ${themeDuration}`} onClick={() => appendDisplay("-")}>
             -
           </button>
-          <button className="numbers" onClick={() => appendDisplay("4")}>
+          <button className={`dark:text-[#b3b6be] ${themeDuration} text-black`} onClick={() => appendDisplay("4")}>
             4
           </button>
-          <button className="numbers" onClick={() => appendDisplay("5")}>
+          <button className={`dark:text-[#b3b6be] ${themeDuration} text-black`} onClick={() => appendDisplay("5")}>
             5
           </button>
-          <button className="numbers" onClick={() => appendDisplay("6")}>
+          <button className={`dark:text-[#b3b6be] ${themeDuration} text-black`} onClick={() => appendDisplay("6")}>
             6
           </button>
-          <button className="text-[#138EA4] bg-[#1F2130]" onClick={() => appendDisplay("*")}>
+          <button className={`text-[#138EA4] bg-[#F8FAF9] dark:bg-[#1F2130] ${themeDuration}`} onClick={() => appendDisplay("*")}>
             *
           </button>
-          <button className="numbers" onClick={() => appendDisplay("1")}>
+          <button className={`dark:text-[#b3b6be] ${themeDuration} text-black`} onClick={() => appendDisplay("1")}>
             1
           </button>
-          <button className="numbers" onClick={() => appendDisplay("2")}>
+          <button className={`dark:text-[#b3b6be] ${themeDuration} text-black`} onClick={() => appendDisplay("2")}>
             2
           </button>
-          <button className="numbers" onClick={() => appendDisplay("3")}>
+          <button className={`dark:text-[#b3b6be] ${themeDuration} text-black`} onClick={() => appendDisplay("3")}>
             3
           </button>
-          <button className="text-[#138EA4]  bg-[#1F2130]" onClick={() => appendDisplay("/")}>
+          <button className={`text-[#138EA4] bg-[#F8FAF9] dark:bg-[#1F2130] ${themeDuration}`} onClick={() => appendDisplay("/")}>
             /
           </button>
-          <button className="numbers" onClick={() => appendDisplay("0")}>
+          <button className={`dark:text-[#b3b6be] ${themeDuration} text-black`} onClick={() => appendDisplay("0")}>
             0
           </button>
-          <button className="numbers" onClick={() => appendDisplay(".")}>
+          <button className={`dark:text-[#b3b6be] ${themeDuration} text-black`} onClick={() => appendDisplay(".")}>
             .
           </button>
-          <button className="numbers" onClick={logValues}>
+          <button className={`dark:text-[#b3b6be] ${themeDuration} text-black`} onClick={logValues}>
             log
           </button>
           <button className="text-[#A5FFFF] bg-[#00BFDE]" onClick={clacDisplay}>
